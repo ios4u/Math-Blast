fc09af41fc67202e835ec0919369dc24350e7d99
Jonny Ramos <jramos@uchicago.edu>
2013-07-05 15:02:50 -0500

    Tutorial

---
+++ MathBlast/Classes/Tutorial/Tutorial.h
@@ -0,0 +1,16 @@
+//
+//  Tutorial.h
+//  MathBlast
+//
+//  Created by JRamos on 7/5/13.
+//  Copyright 2013 JRamos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Tutorial : CCScene {
+    
+}
+
+@end
---
+++ MathBlast/Classes/Tutorial/Tutorial.m
@@ -0,0 +1,14 @@
+//
+//  Tutorial.m
+//  MathBlast
+//
+//  Created by JRamos on 7/5/13.
+//  Copyright 2013 JRamos. All rights reserved.
+//
+
+#import "Tutorial.h"
+
+
+@implementation Tutorial
+
+@end
